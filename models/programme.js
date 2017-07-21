896d74ebf8498f29f43958bb4b39072104256967
Pedro Martin <pataruco@gmail.com>
2017-07-21 21:48:35 +0100

    Model programme should return an object

---
+++ models/programme.js
@@ -0,0 +1,10 @@
+// to clean up the data structure
+class Programme {
+    // get raw data
+    constructor(data) {
+        let programme = { }
+        return programme;
+    }
+}
+
+module.exports = Programme;
---
+++ tests/models/programme-test.js
@@ -0,0 +1,44 @@
+const should = require("chai").should();
+const expect = require("chai").expect;
+const assert = require("chai").assert;
+const Programme = require('../../models/programme');
+
+const data = {
+    "title": "'arse that Jack Built, The",
+    "slice_title": "arsethatjackbuiltthe",
+    "letter": "a",
+    "programme": {
+        "type": "episode",
+        "pid": "b01ljwm4",
+        "title": "The 'arse that Jack Built",
+        "short_synopsis": "Ian McMillan goes in search of one of Britain's strangest linguistic features.",
+        "image": {
+            "pid": "p01hwgzc"
+        },
+        "position": null,
+        "expected_child_count": null,
+        "first_broadcast_date": "2012-08-10T11:00:00+01:00",
+        "is_available": true,
+        "ownership": {
+            "service": {
+                "type": "radio",
+                "id": "bbc_radio_four",
+                "key": "radio4",
+                "title": "BBC Radio 4"
+            }
+        }
+    }
+}
+
+describe('Model Programme', () => {
+
+    const programme = new Programme(data);
+
+    describe('instantiation', () => {
+
+        it('should be an object', (done) => {
+            assert.isObject(programme);
+            done();
+        });
+    });
+});
