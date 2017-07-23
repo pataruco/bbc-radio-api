ba49ec78816174fa9c14f0523d7bd0e5ce2f78af
Pedro Martin <pataruco@gmail.com>
2017-07-23 14:28:18 +0100

    Create a Vue Component
    Handle search suggestion items rendering

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+const Vue = require('vue');
+const axios = require('axios');
+
+window.onload = function( ) {
+    let search = new Vue({
+        el: '#search',
+        data: {
+            programmes: [],
+            noSuggestions: false,
+            searchString: '',
+            showResults: false
+        },
+        methods: {
+            getSuggestions: function( ) {
+                let searchString = this.searchString;
+                if ( searchString !== '') {
+                    return axios.get(`/suggest=${searchString}`)
+                    .then( ( response ) => {
+                        this.programmes = response.data;
+                        this.showNoResults( this.programmes );
+                        this.showResults = true;
+                    });
+                }
+                return this.showResults = false;
+            },
+
+            showNoResults: function ( programmes ) {
+                if ( programmes.length  <= 0 ) {
+                    return this.noSuggestions = true;
+                }
+                return this.noSuggestions = false;
+
+            }
+        }
+    });
+}
